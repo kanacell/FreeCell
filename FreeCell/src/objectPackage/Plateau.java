54ceb2d5bb8f36eefec594f59a7afbd1b6e57606
Kévin <Kvin@192.168.1.14>
2015-05-23 22:24:52 +0200

    Ajout de methodes "classiques" aux différents objets

--- FreeCell/src/objectPackage/Plateau.java
+++ FreeCell/src/objectPackage/Plateau.java
@@ -1,6 +1,7 @@
 package objectPackage;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class Plateau {
 	private Colonne[] plateau;
@@ -19,6 +20,14 @@ public class Plateau {
 		}
 	}
 	
+	public Plateau clone (){
+		Plateau renvoi = new Plateau(this.plateau.length);
+		for (int i = 0; i < renvoi.plateau.length; i++){
+			renvoi.plateau[i] = this.plateau[i].clone();
+		}
+		return renvoi;
+	}
+	
 	/*
 	 * Classe Interne privee au plateau
 	 */
@@ -29,6 +38,31 @@ public class Plateau {
 		 */
 		public Colonne (){
 			super();
+		}
+		
+		public String toString (){
+			String chaine_resultat = "Colonne : { ";
+			ListIterator<Carte> iterateurColonne = this.listIterator();
+			while ( iterateurColonne.hasNext() ){
+				chaine_resultat += iterateurColonne.next().toString() + "; ";
+			}
+			return chaine_resultat;
+		}
+		
+		public boolean equals (Colonne otherColonne){
+			boolean identique;
+			identique = ( this.size() == otherColonne.size() );
+			int i = 0;
+			while ( i < this.size() && identique ){
+				identique = identique && this.get(i).equals(otherColonne.get(i));
+			}
+			return identique;
+		}
+		
+		public Colonne clone (){
+			Colonne renvoi = new Colonne();
+			renvoi.addAll(this);
+			return renvoi;
 		}
 	}
 	
--- FreeCell/src/objectPackage/Table.java
+++ FreeCell/src/objectPackage/Table.java
@@ -12,4 +12,29 @@ public class Table {
 		zoneDeJeu = new Plateau (Constantes.Plateau.nombreColonnes);
 		zoneDeRangement = new Plateau (Constantes.Plateau.nombreFamilles);
 	}
+	
+	/*
+	 * 3 ACCESSEURS
+	 */
+	public Stock getStock (){
+		return zoneDeStockage;
+	}
+	public Plateau getZoneDeJeu (){
+		return zoneDeJeu;
+	}
+	public Plateau getZoneDeRangement (){
+		return zoneDeRangement;
+	}
+	
+	public void setStock (Stock newStock){
+		zoneDeStockage = newStock.clone();
+	}
+	public void setZoneDeJeu (Plateau newZoneDeJeu){
+		zoneDeJeu = newZoneDeJeu.clone();
+	}
+	public void setZoneDeRangement (Plateau newZoneDeRangement){
+		zoneDeRangement = newZoneDeRangement.clone();
+	}
+	
+	
 }
