d6b68a5697530664a5630f33ad980058150a38f7
Kévin <Kvin@192.168.1.14>
2015-05-27 01:03:59 +0200

    Debut d'ajout pour la gestion de la zone principale

--- FreeCell/src/graphiquePackage/GestionSouris.java
+++ FreeCell/src/graphiquePackage/GestionSouris.java
@@ -37,14 +37,12 @@ public class GestionSouris implements MouseListener, MouseMotionListener{
 		Point clic = e.getPoint();
 		switch (typeDeZone){
 		case Constantes.Panneau.zoneDeStockage:
-			System.out.println("pressed sur panneau de stockage");
+			System.out.println("\tpressed sur panneau de stockage");
 			Stock referenceStockage = engi.getZoneStockage();
-			
 			int numeroCarte = estSurStock(clic);
+			
 			if (numeroCarte != -1){
-//				if (pan.getCarteSelectionnee() == -1){
-					pan.selectionnerCarte(numeroCarte);
-//				}
+				pan.selectionnerCarte(numeroCarte);
 				System.out.println("carte : " + referenceStockage.getCarteAt(numeroCarte).toString() );
 				engi.setCarteCourante(referenceStockage.getCarteAt(numeroCarte).clone());
 				engi.setTypePanneauDepart(Constantes.Panneau.zoneDeStockage);
@@ -53,37 +51,27 @@ public class GestionSouris implements MouseListener, MouseMotionListener{
 				if (pan.getCarteSelectionnee() != -1){
 					pan.deselectionnerCarte();
 					engi.getCarteCourante().clear();
+					engi.setTypePanneauDepart(Constantes.Panneau.zoneNulle);
 				}
-			}
-			
-			
-			
-			
-			
-			if ( engi.getTypePanneauDepart() == -1 ){
-				engi.setTypePanneauDepart(Constantes.Panneau.zoneDeStockage);
-			}
-			else {
-				engi.setTypePanneauDepart(Constantes.Panneau.zoneNulle);
 			}
 			
 //			System.out.println(engi.getZoneStockage().toString());
 			break;
 		case Constantes.Panneau.zoneDeJeu:
-			System.out.println("pressed sur panneau de zone principale");
+			System.out.println("\tpressed sur panneau de zone principale");
 			Plateau referencePrincipale = engi.getZonePrincipale();
+			int numeroColonne = estSurPrincipale(clic);
 			
-			if ( engi.getTypePanneauDepart() != Constantes.Panneau.zoneDeJeu ){
+			if ( engi.getTypePanneauDepart() == Constantes.Panneau.zoneNulle ){
 				engi.setTypePanneauDepart(Constantes.Panneau.zoneDeJeu);
 			}
 			else {
-				engi.setTypePanneauDepart(Constantes.Panneau.zoneNulle);
 			}
 			
 			System.out.println(engi.getZonePrincipale().toString());
 			break;
 		case Constantes.Panneau.zoneDeRangement:
-			System.out.println("pressed sur panneau de rangement");
+			System.out.println("\tpressed sur panneau de rangement");
 			Plateau referenceRangement = engi.getZoneRangement();
 			
 			if ( engi.getTypePanneauDepart() != Constantes.Panneau.zoneDeRangement ){
@@ -131,10 +119,12 @@ public class GestionSouris implements MouseListener, MouseMotionListener{
 	public void mouseEntered(MouseEvent e) {}
 	public void mouseExited(MouseEvent e) {}
 	
+	/*
+	 * Methodes Private de GestionSouris
+	 */
 	private int estSurStock (Point clic){
 		int numeroCarte = -1;
-		int borneGauche = 15;
-		int borneDroite = borneGauche + Constantes.Panneau.largeurCarte;
+		int borneGauche = 20,  borneDroite = borneGauche + Constantes.Panneau.largeurCarte;
 		int borneSuperieure = 20;
 		int borneInferieure = borneSuperieure + Constantes.Panneau.hauteurCarte;
 		if (clic.x >= borneGauche && clic.x <= borneDroite 
@@ -157,4 +147,57 @@ public class GestionSouris implements MouseListener, MouseMotionListener{
 		return numeroCarte;
 	}
 
+	private int estSurPrincipale (Point clic){
+		int numeroColonne = 0;
+		int borneGauche = 15, borneDroite = borneGauche + Constantes.Panneau.largeurCarte;
+		if (clic.x >= borneGauche && clic.x <= borneDroite 
+		&&	estSurZoneCliquable(clic, engi.getZonePrincipale(), numeroColonne++) ){
+			numeroColonne = 0;
+		}
+		else if (clic.x >= (borneGauche = borneDroite + 10) && clic.x <= (borneDroite = borneGauche + Constantes.Panneau.largeurCarte)
+			&&	 estSurZoneCliquable(clic, engi.getZonePrincipale(), numeroColonne++) ){
+			numeroColonne = 1;
+		}
+		else if (clic.x >= (borneGauche = borneDroite + 10) && clic.x <= (borneDroite = borneGauche + Constantes.Panneau.largeurCarte)
+			&&	 estSurZoneCliquable(clic, engi.getZonePrincipale(), numeroColonne++) ){
+			numeroColonne = 2;
+		}
+		else if (clic.x >= (borneGauche = borneDroite + 10) && clic.x <= (borneDroite = borneGauche + Constantes.Panneau.largeurCarte)
+			&&	 estSurZoneCliquable(clic, engi.getZonePrincipale(), numeroColonne++) ){
+			numeroColonne = 3;
+		}
+		else if (clic.x >= (borneGauche = borneDroite + 10) && clic.x <= (borneDroite = borneGauche + Constantes.Panneau.largeurCarte)
+			&&	 estSurZoneCliquable(clic, engi.getZonePrincipale(), numeroColonne++) ){
+			numeroColonne = 4;
+		}
+		else if (clic.x >= (borneGauche = borneDroite + 10) && clic.x <= (borneDroite = borneGauche + Constantes.Panneau.largeurCarte)
+			&&	 estSurZoneCliquable(clic, engi.getZonePrincipale(), numeroColonne++) ){
+			numeroColonne = 5;
+		}
+		else if (clic.x >= (borneGauche = borneDroite + 10) && clic.x <= (borneDroite = borneGauche + Constantes.Panneau.largeurCarte)
+			&&	 estSurZoneCliquable(clic, engi.getZonePrincipale(), numeroColonne++) ){
+			numeroColonne = 6;
+		}
+		else if (clic.x >= (borneGauche = borneDroite + 10) && clic.x <= (borneDroite = borneGauche + Constantes.Panneau.largeurCarte)
+			&&	 estSurZoneCliquable(clic, engi.getZonePrincipale(), numeroColonne) ){
+			numeroColonne = 7;
+		}
+		else {
+			numeroColonne = -1;
+		}
+		
+		return numeroColonne;
+	}
+	
+	private boolean estSurZoneCliquable (Point clic, Plateau zonePrincipale, int numeroColonne){
+		System.out.println("numeroColonne : " + numeroColonne);
+		boolean clicValide;
+		int compteurCarte = ( zonePrincipale.getColonneAt(numeroColonne).size() > 0 ) ? zonePrincipale.getColonneAt(numeroColonne).size() : 1;
+		int borneSuperieure = 20;
+		int borneInferieure = borneSuperieure + (compteurCarte-1)*20 + Constantes.Panneau.hauteurCarte;
+		
+		clicValide = clic.y >= borneSuperieure && clic.y <= borneInferieure; 
+		
+		return clicValide;
+	}
 }
